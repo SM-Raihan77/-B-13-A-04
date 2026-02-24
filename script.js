f84e3a19f16c629aa646032ba644913d9b2b9e7a
SM-Raihan77 <smraihan720@gmail.com>
2026-02-24 06:21:14 +0600

    add toggle style

--- script.js
+++ script.js
@@ -12,11 +12,50 @@
         const filterSection = document.getElementById('filtered-section');
 
         // --- Step 02: Calculate Count ---
-        
+
         function calculateCount() {
             totalDisplay.innerText = allCardSection.children.length;
             interviewDisplay.innerText = interviewList.length;
             rejectedDisplay.innerText = rejectedList.length;
+        
         }
         calculateCount();
+
+        
+        // --- Step 3: Tab Toggle logic ---
+        function toggleStyle(id) {
+
+            // Style reset
+
+            const tabs = ['tab-all', 'tab-interview', 'tab-rejected'];
+            tabs.forEach(tabId => {
+                const btn = document.getElementById(tabId);
+                btn.classList.replace('bg-blue-600', 'bg-white');
+                btn.classList.remove('text-white');
+            });
+
+            // Active style add
+
+            const selected = document.getElementById(id);
+            selected.classList.replace('bg-white', 'bg-blue-600');
+            selected.classList.add('text-white');
+            
+            currentStatus = id;
+
+            // Section show/hide
+            
+            if (id === 'tab-all') {
+                allCardSection.classList.remove('hidden');
+                filterSection.classList.add('hidden');
+            } else if (id === 'tab-interview') {
+                allCardSection.classList.add('hidden');
+                filterSection.classList.remove('hidden');
+                renderInterview();
+            } else if (id === 'tab-rejected') {
+                allCardSection.classList.add('hidden');
+                filterSection.classList.remove('hidden');
+                renderRejected();
+            }
+        }
+
 
